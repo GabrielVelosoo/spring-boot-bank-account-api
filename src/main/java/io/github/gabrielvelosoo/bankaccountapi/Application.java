7c1cb4ffe96d92dbcf42099b8be149d40f4a395b
Gabriel Veloso Pinheiro <tr26660es@trf1.gov.br>
2025-04-02 14:15:09 -0300

    hml: Created Spring Boot Project and Initial Configuration

---
+++ src/main/java/io/github/gabrielvelosoo/bankaccountapi/Application.java
@@ -0,0 +1,22 @@
+package io.github.gabrielvelosoo.bankaccountapi;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(Application.class);
+		builder.bannerMode(Banner.Mode.OFF);
+		builder.run(args);
+		ConfigurableApplicationContext applicationContext = builder.context();
+		ConfigurableEnvironment environment = applicationContext.getEnvironment();
+		String applicationName = environment.getProperty("spring.application.name");
+		System.out.println("Application name: " + applicationName);
+	}
+
+}
